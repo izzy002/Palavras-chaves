6c781c4b205927ea99095c318ace84406b8a8edd
Izzy <isabely.bravo@escola.pr.gov.br>
2025-07-01 00:28:34 +0000

    Aula 04 - removendo caracteres especiais

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
-function mostraPalavrasChave (){
+function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras  = texto.split(" ");
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = palavras.join(", ");
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    return palavras;
 }
